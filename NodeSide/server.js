beb8d9f5e1e4dc95a56b8671e483209063eb1dde
FerrariAndrea <ferrari.andrea.95.b10@gmail.com>
2019-05-08 12:30:50 +0200

    Fix free moemori when close-connection

--- NodeSide/server.js
+++ NodeSide/server.js
@@ -26,13 +26,14 @@ app.listen(PORT, function(){
 var status = {"Temp1": "...","Temp2": "...","cpu":"..."}
 var socketActive = false;
 var clients = [ ];
-var count = 0;
+//var count = 0;
 //----------------------------------------------------------------------HTTP
 function onStart(req, res){//enable sending to everyone
 	console.log("onStart on HTTP");
 	socketActive="true";
 	status["Enable"]="true";
-	status["CountConn"] = count; //status["CountConn"] = clients.length;
+	//status["CountConn"] = count; 
+	status["CountConn"] = clients.length;
 	//send to all client the new status
 	sendStatusToAll();
 	res.status(200).json({"onStart": "ok"});
@@ -43,7 +44,8 @@ function onStop(req, res){//disable sending to everyone
 	//send to all client the new status
 	//just for see Enable=false
 	status["Enable"]="false";
-	status["CountConn"] = count; //status["CountConn"] = clients.length;
+	//status["CountConn"] = count; 
+	status["CountConn"] = clients.length;
 	sendStatusToAll();
 	//than disable it
 	socketActive=false;
@@ -55,7 +57,8 @@ function setStatus(req, res){//set new status
 	//send to all client the new status if sending is enable
 		status =req.body;
 		status["Enable"]=socketActive+"";
-		status["CountConn"] = count; //status["CountConn"] = clients.length;
+		//status["CountConn"] = count; 
+		status["CountConn"] = clients.length;
 		sendStatusToAll();
 	  res.status(200).json({"onStop": "ok"});
 }
@@ -84,47 +87,50 @@ wsServer = new WebSocketServer({
 // WebSocket server
 wsServer.on('request', function(request) {
   var connection = request.accept(null, request.origin);
-  console.log("Accepted connection (socket).");
-  //add this connection to array clients
-  clients.push(connection);
-  //send status if enable
-  /*
-	  if(socketActive){
-			connection.sendUTF( JSON.stringify(status));
-	  }
-  */
+	  console.log("Accepted connection (socket).");
+	  //add this connection to array clients
+	  clients.push(connection);
+	  //console.log(connection);
+	  //send status if enable
+	  /*
+		  if(socketActive){
+				connection.sendUTF( JSON.stringify(status));
+		  }
+	  */
+	  
+	  //send to all for see connection number increse
+	  //count++;
+	  //status["CountConn"] = count; 
+	  status["CountConn"] = clients.length; 
+	  sendStatusToAll() 
   
-  //send to all for see connection number increse
-  count++;
-  status["CountConn"] = count; 
-  sendStatusToAll() 
   
-  
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
-			 /*
-				  connection.on('message', function(message) {
-					if (message.type === 'utf8') {
-					  // process WebSocket message
-					}
-				  });
-			*/
+	  // This is the most important callback for us, we'll handle
+	  // all messages from users here.
+				 /*
+					  connection.on('message', function(message) {
+						if (message.type === 'utf8') {
+						  // process WebSocket message
+						}
+					  });
+				*/
 	//handle close connection
-  connection.on('close', function(connection) {
-    // close user connection	
-	console.log("Close connection (socket).");
-	//remove connection from clients 
-	//DON'T WORK
-	/*
-	var index = clients.indexOf(connection);
-	console.log(index); //-->>> -1  :(
-	if (index > -1) {
-	  clients.splice(index, 1);
-	}
-	*/
-	count--;
-	status["CountConn"] = count; 
-	 //send to all for see connection number decrese
-	sendStatusToAll()
+	connection.on('close', function(connection_closed) {
+		//WARNING connection_closed is not connection
+		//so for free memory of clients array need work with connection
+		//not with connection_closed which is arg of callback "close"
+		// close user connection	
+		console.log("Close connection (socket).");
+		//remove connection from clients (free memory)
+		var index = clients.indexOf(connection);
+			if (index > -1) {
+		  clients.splice(index, 1);
+		}
+		
+		//count--;
+		//status["CountConn"] =count;
+		status["CountConn"] = clients.length; 
+		 //send to all for see connection number decrese
+		sendStatusToAll()
   }); 
 });
